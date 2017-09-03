7686bef3108dd818d4349afd2bd34ecf4a2d2957
Heather OConnell <hoconnell@csumb.edu>
2017-09-03 21:21:10 +0000

    adding hw 1

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
     <body>
         
         <h1>Heather O'Connell's CST 352 Web Scripting</h1>
-        <a href="labs/lab1/portfolio/index.html">Lab 1</a>
+        <h3>Homework</h3>
+        <a href="hw/hw1/index.html">Homework 1: HTML & CSS</a>
+        <h3>Labs</h3>
+        <a href="labs/lab1/portfolio/index.html">Lab 1: HTML & CSS</a>
         
     </body>
 </html>
